c3e2dc54f0831cfebec0a6c2b4817a5212cae5da
Daniel Roe <daniel@roe.dev>
2022-05-15 22:06:57 +0100

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['@nuxtjs/eslint-config-typescript']
+  extends: ['@nuxtjs/eslint-config-typescript'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'import/named': 'off'
+  }
 }
