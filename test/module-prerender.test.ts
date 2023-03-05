a54a91fa2958d15b1ef4c065b036562d24fb9cca
Daniel Roe <daniel@roe.dev>
2023-03-05 15:21:48 +0000

    test: add impenetrable mock 💣

--- test/module-prerender.test.ts
+++ test/module-prerender.test.ts
@@ -5,7 +5,13 @@ import { describe, it, expect, vi } from 'vitest'
 import { setup, useTestContext } from '@nuxt/test-utils'
 import { useNuxt } from '@nuxt/kit'
 
-vi.spyOn(console, 'error')
+const error = vi.fn()
+Object.defineProperty(console, 'error', {
+  get () {
+    return error
+  },
+  set () {}
+})
 
 await setup({
   rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
