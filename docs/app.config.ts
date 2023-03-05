31f09d2fa58843c011da3a7aaf1a4f962c0a5f11
Daniel Roe <daniel@roe.dev>
2023-03-05 14:22:17 +0000

    chore: upgrade docus, migrate to pnpm and use bumpp

---
+++ docs/app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: '@nuxtjs/html-validator',
+    url: 'https://html-validator.nuxtjs.org',
+    description: 'The best place to start your documentation.',
+    socials: {
+      github: 'nuxt-modules/html-validator',
+    },
+    image: 'https://html-validator.nuxtjs.org/preview.png',
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs'
+        }
+      ]
+    }
+  }
+})
---
+++ docs/tokens.config.ts
@@ -0,0 +1,20 @@
+import { defineTheme } from 'pinceau'
+
+// E6F0F0
+
+export default defineTheme({
+  color: {
+    primary: {
+      '50': '#eefbf5',
+      '100': '#d7f4e5',
+      '200': '#b1e9cf',
+      '300': '#7fd6b4',
+      '400': '#41b38a',
+      '500': '#27a27a',
+      '600': '#198262',
+      '700': '#146850',
+      '800': '#125341',
+      '900': '#104436',
+    }
+  }
+})
