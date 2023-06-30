c1d2919de6fd738515906a0fca08ed270c0f4ee1
Julien Huang <julien.huang@outlook.fr>
2023-06-30 11:27:30 +0200

    feat: type nitro hook (#391)

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -1,4 +1,5 @@
 import type { NitroAppPlugin, RenderResponse } from 'nitropack'
+import { getRequestPath } from 'h3'
 import { useChecker, getValidator } from './validator'
 // @ts-expect-error virtual module
 import config from '#html-validator-config'
@@ -7,10 +8,10 @@ export default <NitroAppPlugin> function (nitro) {
   const validator = getValidator(config.options)
   const { checkHTML } = useChecker(validator, config.usePrettier, config.logLevel)
 
-  nitro.hooks.hook('render:response', async (response: RenderResponse, { event }) => {
-    if (typeof response.body === 'string' && (response.headers['Content-Type'] || response.headers['content-type'])?.includes('html')) {
+  nitro.hooks.hook('render:response', async (response: Partial<RenderResponse>, { event }) => {
+    if (typeof response.body === 'string' && (response.headers?.['Content-Type'] || response.headers?.['content-type'])?.includes('html')) {
       // Block the response only if it's not hookable
-      const promise = checkHTML(event.req.url, response.body)
+      const promise = checkHTML(getRequestPath(event), response.body)
       if (config.hookable) {
         await nitro.hooks.callHook('html-validator:check', await promise, response, { event })
       }
