eee2ca999392e29a56a4a8fb5c33351d7016f751
Daniel Roe <daniel@roe.dev>
2024-09-22 21:21:51 +0100

    chore: add `compatibilityDate` field

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,4 @@
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-19',
   extends: '@nuxt-themes/docus',
 })
--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -1,5 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-19',
   modules: ['@nuxtjs/html-validator'],
 })
